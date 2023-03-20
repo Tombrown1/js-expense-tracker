353913a41abc9d9c15c8ea146d5f1cf40955a8f9
Tombrown1 <godwintombrown@gmail.com>
2023-03-20 14:58:15 +0100

    Js Expense App Done

---
+++ index.js
@@ -0,0 +1,133 @@
+const form = document.querySelector(".add");
+const incomeList = document.querySelector("ul.income-list");
+const expenseList = document.querySelector("ul.expense-list");
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+const transactionHistory = document.querySelector(".transaction-history");
+
+let transactions =
+    localStorage.getItem("transactions") !== null
+        ? JSON.parse(localStorage.getItem("transactions"))
+        : []; 
+
+
+function updateStatistics() {
+    const updatedIncome = transactions
+        .filter(transaction => transaction.amount > 0)
+        .reduce((total, transaction) => (total += transaction.amount), 0);
+
+    const updatedExpense = transactions
+        .filter(transaction => transaction.amount < 0)
+        .reduce(
+            (total, transaction) => (total += Math.abs(transaction.amount)),
+            0
+        );
+
+    const updatedBalance = updatedIncome - updatedExpense;
+    balance.textContent = updatedBalance;
+    income.textContent = updatedIncome;
+    expense.textContent = updatedExpense;
+}
+
+updateStatistics();
+
+function generateTemplate(id, source, time, amount) {
+    return `<li data-id="${id}">
+                <p>
+                    <span>${source}</span>
+                    <span id="time"> ${time}</span>
+                </p>
+                $<span>${Math.abs(amount)}</span>
+                <i class="bi bi-trash delete"></i>
+            </li>`;
+}
+
+function addTransactionDom(id, source, time, amount) {
+    if (amount > 0) {
+        incomeList.innerHTML += generateTemplate(id, source, time, amount);
+    } else {
+        expenseList.innerHTML += generateTemplate(id, source, time, amount);
+    }      
+    
+}
+
+function addTransaction(source, amount) {
+    const time = new Date();
+    const transaction = {
+        id: Math.floor(Math.random() * 10000),
+        source: source,
+        amount: amount,
+        time: `${time.toLocaleTimeString()} ${time.toLocaleDateString()}`
+    };
+    transactions.push(transaction);
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    addTransactionDom(Number(transaction.id), source, transaction.time, amount);
+   
+}
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    if (form.source.value.trim() === "" || form.amount.value === "") {
+        return alert("Please type in the correct values");
+    }    
+
+    addTransaction(form.source.value, Number(form.amount.value));
+    updateStatistics();
+    form.reset();
+});
+
+// Function to fetch record from localstorage and display to the user
+function getTransaction() {
+    transactions.forEach(transaction => {
+        if (transaction.amount > 0) {
+            incomeList.innerHTML += generateTemplate(
+                transaction.id,
+                transaction.source,
+                transaction.amount,
+                transaction.amount
+            );
+        } else {
+            expenseList.innerHTML += generateTemplate(
+                transaction.id,
+                transaction.source,
+                transaction.time,
+                transaction.amount
+            );
+        }
+    }); 
+    
+}
+
+function deleteTransaction(id) {
+    transactions = transactions.filter(transaction => {
+        // console.log(transaction.id, id);
+        return transaction.id !== id;
+    });
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+incomeList.addEventListener("click", event => {
+    if (event.target.classList.contains("delete")) {
+        console.log(event.target.parentElement.dataset.id);
+        event.target.parentElement.remove();
+        deleteTransaction(Number(event.target.parentElement.dataset.id));
+        updateStatistics();
+    }
+});
+
+expenseList.addEventListener("click", event => {
+    if (event.target.classList.contains("delete")) {
+        event.target.parentElement.remove();
+        deleteTransaction(Number(event.target.parentElement.dataset.id));
+        updateStatistics();
+    }
+});
+
+function init() {   
+    updateStatistics();
+    getTransaction();   
+}
+
+init();
